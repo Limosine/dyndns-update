a4393a5d6e781fa0f2303ca9574880dd6be4732b
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-02-04 00:18:33 +0100

    Remove data

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='20230129',
     description='Update your DynDNS hosts',
     author='Quentin Frey',
-    author_email='***REMOVED***',
+    author_email='quentin.frey@proton.me',
     url='https://github.com/Limosine/dyndns-update',
     packages=['dyndns_update'],
     install_requires=[
