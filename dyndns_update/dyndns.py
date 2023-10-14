0b8d898963e2b7290c819b8c30863a609e72a203
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-10-14 10:13:03 +0200

    Fix logic

--- dyndns_update/dyndns.py
+++ dyndns_update/dyndns.py
@@ -53,8 +53,7 @@ def getIP(force):
         if ipv4 == ipv4_cache and force != True:
             print("IP hasn't changed.")
             sys.exit()
-        else:
-            ipv4 = ipv4_cache
+        else if force == True:
             ipv6 = ip[1]
     else:
         ipv6 = requests.get("https://api6.ipify.org?format=json").json()['ip']
